9cda164c1af64c917a66144d8ae78584bd49750a
cqxxh <cqxxhht233@163.com>
2026-04-16 20:56:40 +0800

    bolg-0.0

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script>
+import {defineComponent} from "vue";
+import Welcome from "./Welcome.vue";
+
+export default defineComponent({
+    components: {Welcome}
+})
+</script>
+
+<template>
+    <Welcome/>
+</template>
---
+++ frontend/src/Welcome.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="home-top">
+        <div>
+            
+        </div>
+    </div>
+</template>
+
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+</style>
+
+<style scoped>
+.home-top {
+    width: 100%;
+    height: 10vh;
+    background: aqua;
+    display: flex;
+}
+</style>
