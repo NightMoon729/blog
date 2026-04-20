33566e82745e57d15218257ebdbf5965c8e5403b
cqxxh <cqxxhht233@163.com>
2026-04-20 12:00:39 +0800

    bolg-0.2

---
+++ frontend/src/Card/IconCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+.card{
+    padding: 5px 10px;
+    border: 1px black solid;
+    color: black;
+    background: #daf6f4;
+    backdrop-filter: blur(5px);
+}
+
+@media (prefers-color-scheme: dark) {
+    .card{
+        color: rgba(255, 255, 255, 0.8);
+        background: rgba(2, 12, 12, 0.35);
+    }
+}
+</style>
+<script setup lang="ts">
+</script>
